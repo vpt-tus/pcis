3645208cef10997becdba5929e2964f7b0fe4906
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2018-10-19 16:35:26 +0300

    Add comparator model and test

---
+++ examples/simulation/comparator.bat
@@ -0,0 +1,8 @@
+call C:\Xilinx\Vivado\2018.2\settings64.bat
+call xvlog -sv -nolog comparator.sv;
+if ERRORLEVEL 1 (
+  exit
+)
+call xelab -R -s comparator -nolog -debug all comparator_test_1
+del *.jou,*.log
+rem call xsim comparator -gui
