79958fc573a22ef5cda07861e06a41a85b696207
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2018-10-26 12:22:55 +0300

    Add decoder.sv example

---
+++ examples/simulation/decoder.bat
@@ -0,0 +1,8 @@
+call C:\Xilinx\Vivado\2018.2\settings64.bat
+call xvlog -sv -nolog decoder.sv;
+if ERRORLEVEL 1 (
+  exit
+)
+call xelab -R -s decoder -nolog -debug all decoder_test_1
+del *.jou,*.log
+rem call xsim decoder -gui
