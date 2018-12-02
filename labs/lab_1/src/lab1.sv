3b24f7a9a5020e9e3c9ff0bd506208afd29a2e12
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2018-12-02 13:18:18 +0200

    Update README

--- labs/lab_1/src/lab1.sv
+++ labs/lab_1/src/lab1.sv
@@ -11,21 +11,3 @@ module lab1a #(parameter WIDTH = 8) (
 			sr <= {sr[WIDTH-2:0], ~sr[WIDTH-1]};
 
 endmodule
-
-module lab1b #(parameter WIDTH = 8) (
-	input clock, reset,
-	output logic [WIDTH-1:0] sr
-);
-
-	//TODO
-
-endmodule
-
-module lab1c #(parameter WIDTH = 8) (
-	input clock, reset,
-	output logic [WIDTH-1:0] sr
-);
-
-	// TODO
-
-endmodule
