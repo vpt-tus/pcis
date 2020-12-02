401e0fbd32bc8e9260c98b8d002315a381bb70a4
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2020-12-02 18:06:15 +0200

    Code cleanup

--- examples/timer-switch/src/timer_switch_test.sv
+++ examples/timer-switch/src/timer_switch_test.sv
@@ -1,5 +1,5 @@
 module timer_switch_test;
-  localparam T = 10;
+  localparam T = 20;
 
   logic clock = 0;
   logic reset, btn, light;
@@ -17,31 +17,36 @@ module timer_switch_test;
     #T btn = 1;
     #T btn = 0;
 
-    #(25*T) btn = 1;
+    #(50*T) btn = 1;
     #T btn = 0;
 
     #(10*T) btn = 1;
-    #(T) btn = 0;
+    #T btn = 0;
     
     #(30*T) $finish();
   end
 
-  // Check state after reset
-  assert property(@(posedge clock) reset |-> uut.state == uut.OFF and ~light);
+  initial begin
+    $timeformat(-9,0,"ns",5);
+    $monitor("%t %b %b %3s %d", $realtime, btn, light, uut.state.name, uut.timer);
+  end
+
+  // After reset, the FSM is in state OFF and the signal light is '0'
+  assert property(@(posedge clock) reset |=> uut.state == uut.OFF and ~light);
   
   // Pressing the button turns on the lights
+  assert property(@(posedge clock) ~btn ##1 btn |=> light);
+  // Alternative using sampled value function $rose
   assert property(@(posedge clock) $rose(btn) |=> light);
-
-  // Check ON duration
+  // Alternative with min ON duration check
   property light_is_on_for_at_least_20cycles;
     @(posedge clock) $rose(btn) |=> light [*20];
   endproperty;
   assert property (light_is_on_for_at_least_20cycles);
-  
    
   // Check ON duration - alternative
   property light_remains_on_exactly_20cycles_after_the_last_btn_rise;
-    @(posedge clock) disable iff (reset) $fell(light) |-> $past(btn, 21);
+    @(posedge clock) disable iff (reset) $fell(light) |-> $past(btn, 21) and $past(~btn, 22);
   endproperty;
   assert property (light_remains_on_exactly_20cycles_after_the_last_btn_rise);
 
--- examples/traffic-light/src/tlight_test.sv
+++ examples/traffic-light/src/tlight_test.sv
@@ -21,7 +21,7 @@ module tlight_test;
   property after_reset_state;
     @(posedge clock) $fell(reset) |-> uut.state == uut.RESET;
   endproperty;
-  ap_after_reset_state: assert property(after_reset_state);
+  assert property(after_reset_state);
 
   //
   // Check state duration and next state transition
@@ -31,59 +31,63 @@ module tlight_test;
   property check_WE_READY_TO_GO_duration;
     @(posedge clock) uut.state == uut.RESET ##1 uut.state == uut.WE_READY_TO_GO |-> uut.state==uut.WE_READY_TO_GO [*3] ##1 uut.state==uut.WE_GO;
   endproperty;
-  ap_check_WE_READY_TO_GO_duration: assert property(check_WE_READY_TO_GO_duration);
+  assert property(check_WE_READY_TO_GO_duration);
 
   // state: WE_READY_TO_GO duration: 3s - alternative
   // The system function $past returns the value of an expression in a previous clock cycle.
   property check_WE_READY_TO_GO_duration_alt;
     @(posedge clock) uut.state == uut.WE_READY_TO_GO ##1 uut.state == uut.WE_GO |-> $past(uut.state==uut.WE_READY_TO_GO,2);
   endproperty;
-  ap_check_WE_READY_TO_GO_duration_alt: assert property(check_WE_READY_TO_GO_duration_alt);
+  assert property(check_WE_READY_TO_GO_duration_alt);
 
   // state: WE_GO duration: 15s
   // transition: WE_GO -> WE_PREPARE_TO_STOP  
   property check_WE_GO_duration;
     @(posedge clock) uut.state == uut.WE_READY_TO_GO ##1 uut.state == uut.WE_GO |-> uut.state==uut.WE_GO [*15] ##1 uut.state==uut.WE_PREPARE_TO_STOP;
   endproperty;
-  ap_check_WE_GO_duration: assert property(check_WE_GO_duration);
+  assert property(check_WE_GO_duration);
 
   // state: WE_PREPARE_TO_STOP duration: 1s
   // transition: WE_PREPARE_TO_STOP -> NS_READY_TO_GO
   property check_WE_PREPARE_TO_STOP_duration;
     @(posedge clock) uut.state == uut.WE_GO ##1 uut.state == uut.WE_PREPARE_TO_STOP |-> uut.state==uut.WE_PREPARE_TO_STOP ##1 uut.state==uut.NS_READY_TO_GO;
   endproperty;
-  ap_check_WE_PREPARE_TO_STOP_duration: assert property(check_WE_PREPARE_TO_STOP_duration);
+  assert property(check_WE_PREPARE_TO_STOP_duration);
 
   // state: NS_READY_TO_GO duration: 3s
   // transition NS_READY_TO_GO -> NS_GO
   property check_NS_READY_TO_GO_duration;
     @(posedge clock) uut.state == uut.WE_PREPARE_TO_STOP ##1 uut.state == uut.NS_READY_TO_GO |-> uut.state==uut.NS_READY_TO_GO [*3] ##1 uut.state==uut.NS_GO; 
   endproperty;
-  ap_check_NS_READY_TO_GO_duration: assert property(check_NS_READY_TO_GO_duration);
+  assert property(check_NS_READY_TO_GO_duration);
 
   // state: NS_GO duration: 15s
   // transition NS_GO -> NS_PREPARE_TO_STOP
   property check_NS_GO_duration;
     @(posedge clock) uut.state == uut.NS_READY_TO_GO ##1 uut.state == uut.NS_GO |-> uut.state==uut.NS_GO [*15] ##1 uut.state==uut.NS_PREPARE_TO_STOP; 
   endproperty;
-  ap_check_NS_GO_duration: assert property(check_NS_GO_duration);
+  assert property(check_NS_GO_duration);
 
   // Verify state -> lights relations
   ap_check_lights_RESET: assert property
     ( @(posedge clock) uut.state==uut.RESET |-> ns==RED and we==RED);
+
   ap_check_lights_WE_READY_TO_GO: assert property
     ( @(posedge clock) uut.state==uut.WE_READY_TO_GO |-> ns==RED and we==YELLOW);
+
   ap_check_lights_WE_GO: assert property
     ( @(posedge clock) uut.state==uut.WE_GO |-> ns==RED and we==GREEN);
+
   ap_check_lights_WE_PREPARE_TO_STOP: assert property
     ( @(posedge clock) uut.state==uut.WE_PREPARE_TO_STOP |-> ns==RED and we==YELLOW);
+
   ap_check_lights_NS_READY_TO_GO: assert property
     ( @(posedge clock) uut.state==uut.NS_READY_TO_GO |-> ns==YELLOW and we==RED);
+
   ap_check_lights_NS_GO: assert property
     ( @(posedge clock) uut.state==uut.NS_GO |-> ns==GREEN and we==RED);
 
-  initial 
-  begin
+  initial begin
     $display("%20s%6s%7s%7s","STATE","TIMER","WE","NS");
     $monitor("%20s%6d%7s%7s", uut.state.name, uut.timer, ns.name, we.name);
   end
